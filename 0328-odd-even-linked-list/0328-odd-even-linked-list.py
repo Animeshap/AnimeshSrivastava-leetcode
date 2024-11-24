26a29537a1302ca45ba13f145a76a2cb146e8b06
Animeshap <143740103+Animeshap@users.noreply.github.com>
2024-11-24 21:23:28 +0530

    Time: 0 ms (100.00%), Space: 18.2 MB (62.62%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -7,26 +7,13 @@ class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head==None:
             return None
-        temp=head
-        #handle odd cases
-        list_val=[]
-        while temp!=None and temp.next!=None:
-            list_val.append(temp.val)
-            temp=temp.next.next
-        if temp!=None:
-            list_val.append(temp.val)
-        #handle even cases
-        temp=head.next
-        while temp!=None and temp.next!=None:
-            list_val.append(temp.val)
-            temp=temp.next.next
-        if temp!=None:
-            list_val.append(temp.val)
-        #append all values arr to linked list
-        i=0
-        temp=head
-        while temp:
-            temp.val = list_val[i]
-            temp = temp.next
-            i += 1
+        odd=head
+        even=head.next
+        evenhead=head.next
+        while even !=None and even.next!=None:
+            odd.next=odd.next.next
+            even.next=even.next.next
+            odd=odd.next
+            even=even.next
+        odd.next=evenhead
         return head
