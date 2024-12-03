929ecea2e787926ab90f135be5ac3635b939733c
Animeshap <143740103+Animeshap@users.noreply.github.com>
2024-12-04 02:11:40 +0530

    Time: 0 ms (100.00%), Space: 17.4 MB (30.39%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -1,7 +1,40 @@
 class Solution:
+    def lps(self, pat):
+        # Compute the longest prefix-suffix (LPS) array for the KMP algorithm
+        lps = [0] * len(pat)
+        length = 0
+        i = 1
+        while i < len(pat):
+            if pat[i] == pat[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        return lps
+
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
-        words=sentence.split()
-        for i,word in enumerate(words):
-            if word.startswith(searchWord):
-                return i+1
-        return -1
+        words = sentence.split()  # Split the sentence into words
+        for index, word in enumerate(words, start=1):
+            # Use KMP algorithm to check for `searchWord` as a prefix
+            lps = self.lps(searchWord)
+            i = 0
+            j = 0
+            while i < len(word):
+                if word[i] == searchWord[j]:
+                    i += 1
+                    j += 1
+                if j == len(searchWord):  # Found a match
+                    if i == j:  # Match occurs at the beginning of the word
+                        return index
+                    j = lps[j - 1]
+                elif i < len(word) and word[i] != searchWord[j]:
+                    if j != 0:
+                        j = lps[j - 1]
+                    else:
+                        i += 1
+        return -1  # No match found
