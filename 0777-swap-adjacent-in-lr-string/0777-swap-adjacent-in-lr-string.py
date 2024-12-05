46c630bbd5abd18471f6f244581fca561ab43c3e
Animeshap <143740103+Animeshap@users.noreply.github.com>
2024-12-05 16:51:49 +0530

    Time: 7 ms (82.93%), Space: 17.5 MB (15.38%) - LeetHub

---
+++ 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canTransform(self, start: str, target: str) -> bool:
+        i=0
+        j=0
+        n=len(start)
+        blank='X'
+        left='L'
+        right='R'
+        while i<n or j<n:
+            while  i<n and start[i]==blank:
+                i+=1
+            while j<n and target[j]==blank:
+                j+=1
+            if i==n or j==n:
+                break
+            if start[i]!=target[j]:
+                return False
+            if start[i]==left and i<j:
+                return False
+            if start[i]==right and i>j:
+                return False
+            i+=1
+            j+=1
+        return i==n and j==n 
